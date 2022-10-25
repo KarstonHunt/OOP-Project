5bad6c265fcb14b8d81cc0f36ef60b56c02b7d75
KarstonHunt <KarstonHunt@protonmail.com>
2022-10-25 00:09:40 -0700

    Formatting

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ class Item:
     def calculate_total_price(self, x, y):
         return x * y
 
+user_input_item_name = input("What is the item name? ")
+user_input_item_price = input("What is the item price? ")
+user_input_item_quantity = input("What is the item quantity? ")
+
 item1 = Item("Phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 3)
 
 item3 = Item("Desktop", 1500, 5)
 
-user_input_item_name = input("What is the item name? ")
-user_input_item_price = input("What is the item price? ")
-user_input_item_quantity = input("What is the item quantity? ")
-
 item4 = Item(user_input_item_name, user_input_item_price, user_input_item_quantity)
+
+
+
+
