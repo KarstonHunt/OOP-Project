875496adbcdaac90b5ea61dda29177eb82ef9412
KarstonHunt <KarstonHunt@protonmail.com>
2022-10-24 23:58:25 -0700

    created __init__ method with parameters name, price, quantity

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 class Item:
+    def __init__(self, name, price, quantity):
+        print(f"An instance created for: {name}\nPrice: {price}\nQuantity: {quantity}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
     def calculate_total_price(self, x, y):
         return x * y
 
+item1 = Item("Phone", 100, 5)
 
+item2 = Item("Laptop", 1000, 3)
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
-
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item3 = Item("Desktop", 1500, 5)
 
 
